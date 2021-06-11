d004ff19b8e31b4b97357bfe77c8ea27b0a6d2a4
Arohee123 <84009418+Arohee123@users.noreply.github.com>
2021-06-11 21:14:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var bgImg;
+function preload() {
+    //load the images here
+bgImg = loadImage("garden.png")
+}
+
+function setup(){
+    createCanvas(800,800);
+    //create tom and jerry sprites here
+
+}
+
+function draw() {
+
+    background(255);
+    //Write condition here to evalute if tom and jerry collide
+
+    drawSprites();
+}
+
+
+function keyPressed(){
+
+  //For moving and changing animation write code here
+
+
+}
